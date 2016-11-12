421fa9c71735c09ae76780bf9962f13baff165b9
Ethanz <lz306@cs.rutgers.edu>
2016-11-12 00:08:32 -0500

    dataset

--- midterm/dataset.py
+++ midterm/dataset.py
@@ -5,7 +5,7 @@ import math
 import networkx as nx
 from scipy.spatial import distance
 from sklearn.decomposition import PCA
-from sklearn.covariance import GraphLasso
+from sklearn.covariance import GraphLassoCV
 
 def sample(i, j):
     pr = (1/math.sqrt(2*math.pi))*math.exp(-4*((a[i]-b[i])**2 + (a[j]-b[j])**2))
@@ -51,42 +51,51 @@ for i in range(20):
         if adj[i][k] == 1:
             G.add_edge(i, k)
 
-plt.figure(figsize=(12, 12))
-plt.subplot(221)
+fig, ax = plt.subplots(1,1,figsize=(10,10))
 nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_color='red')
 #print(precision, covariance, a, b)
-plt.axis([0,1,0,1])
-plt.axis('on')
-plt.title('Adjacency graph')
+ax.set_axis_on()
+ax.set_title('Adjacency graph')
 
 #plt.show()
 
-plt.matshow(precision, cmap=plt.cm.gray)
-plt.title("True precision matrix")
+fig, ax = plt.subplots(1,4,figsize=(20,8))
+ax[0].set_axis_off()
+ax[1].set_axis_off()
+ax[2].set_axis_off()
+ax[3].set_axis_off()
+ax[1].matshow(precision, cmap=plt.cm.gray)
+ax[1].set_title("True precision matrix")
 #plt.show()
-plt.matshow(covariance, cmap=plt.cm.gray)
-plt.title("True covariance matrix")
+ax[0].matshow(covariance, cmap=plt.cm.gray)
+ax[0].set_title("True covariance matrix")
 #plt.show()
 
 # Create Dataset
 mean = [1.5]*20
 data = np.random.multivariate_normal(mean, covariance, 1000)
-cov = np.cov(data)
-plt.matshow(cov, cmap=plt.cm.gray)
-plt.title("Sample covariance matrix")
-plt.matshow(np.asmatrix(cov).I, cmap=plt.cm.gray)
-plt.title("Sample precision matrix")
+cov = np.cov(data, rowvar=False)
+ax[2].matshow(cov, cmap=plt.cm.gray)
+ax[2].set_title("Sample covariance matrix")
+ax[3].matshow(np.asmatrix(cov).I, cmap=plt.cm.gray)
+ax[3].set_title("Sample precision matrix")
+fig.tight_layout()
 #plt.show()
 
 # PCA
 pca = PCA(n_components=0.95)
 X = pca.fit(data[:750])
-plt.matshow(X.components_, cmap=plt.cm.gray)
-plt.title("Principal directions of the data")
-plt.matshow(X.get_covariance(), cmap=plt.cm.gray)
-plt.title("Estimated covariance matrix")
-plt.matshow(X.get_precision(), cmap=plt.cm.gray)
-plt.title("Inverse covariance matrix")
+fig, ax = plt.subplots(1,3, figsize=(18, 8))
+ax[0].set_axis_off()
+ax[1].set_axis_off()
+ax[2].set_axis_off()
+ax[0].matshow(X.components_, cmap=plt.cm.gray)
+ax[0].set_title("Principal directions of the data")
+ax[1].matshow(X.get_covariance(), cmap=plt.cm.gray)
+ax[1].set_title("Estimated covariance matrix from PCA")
+ax[2].matshow(X.get_precision(), cmap=plt.cm.gray)
+ax[2].set_title("Inverse covariance matrix from PCA")
+fig.tight_layout()
 train_reconstructed = X.inverse_transform(X.transform(data[:750]))
 test_reconstructed = X.inverse_transform(X.transform(data[750:]))
 train_error = []
@@ -105,6 +114,15 @@ plt.title("Reconstruction error for test set")
 # PPCA
 
 # GraphLasso
+gl = GraphLassoCV()
+fig, ax = plt.subplots(1,2,figsize=(12,8))
+gl.fit(data[:750])
+ax[0].set_axis_off()
+ax[0].matshow(gl.precision_, cmap=plt.cm.gray)
+ax[0].set_title("Adjacency matrix by GraphicLasso")
+ax[1].set_axis_off()
+ax[1].matshow(gl.covariance_, cmap=plt.cm.gray)
+ax[1].set_title("Covariance matrix by GraphicLasso")
+plt.tight_layout()
 
-# PPCA
 plt.show()
